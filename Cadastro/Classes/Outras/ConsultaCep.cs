6b7e18f4676ba46741f0efbf7ab5e2bcf940cbd4
Matheus <Matheus@MATHEUS>
2017-07-02 15:36:44 -0300

    Commit Aula 1 - Desing Patterns

---
+++ Cadastro/Classes/Outras/ConsultaCep.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Correios.Net;
+
+namespace Cadastro.Classes
+{
+    public class ConsultaCep
+    {
+        public bool consultarCep(string cep)
+        {
+            if (!string.IsNullOrWhiteSpace(cep))
+            {
+                Address endereco = SearchZip.GetAddress(cep);
+                if (endereco.Zip != null)
+                {
+                    // Conseguiu buscar
+                    return true;
+                   
+                }
+                else
+                {
+                    // Ocorreu um erro na busca
+                    return false;
+                }
+            }
+            else
+            {
+                // Ocorreu um erro na busca - Cep Inválido
+                return false;
+            }
+        }
+
+        /*
+         * Como usar?
+         * 
+            txtEstado.Text = endereco.State;
+            txtCidade.Text = endereco.City;
+            txtBairro.Text = endereco.District;
+            txtRua.Text = endereco.Street;
+        *
+        */
+
+    }
+}
---
+++ CursoDesingPatterns/CalculadorDeImpostos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesingPatterns
+{
+    public class CalculadorDeImpostos
+    {
+        public void RealizaCalculo (Orcamento orcamento, Imposto imposto)
+        {
+            /*
+            if ("IMCS".Equals(imposto))
+            {
+                double icms = orcamento.Valor * 0.01;
+                Console.WriteLine(icms);
+            }
+
+            if("ISS".Equals(imposto))
+            {
+                double iss = orcamento.Valor * 0.06;
+                Console.WriteLine(iss);
+            }
+            */
+
+            /*
+            if ("IMCS".Equals(imposto))
+            {
+                double icms = new ICMS().CalculaICMS(orcamento);
+                Console.WriteLine(icms);
+            }
+
+            if ("ISS".Equals(imposto))
+            {
+                double iss = new ISS().CalculaISS(orcamento);
+                Console.WriteLine(iss);
+            }
+            */
+
+            // o tipo de imposto pode variar passando pelo parametro
+            double tributo = imposto.Calcula(orcamento);
+            Console.WriteLine(tributo);
+
+        }
+    }
+}
